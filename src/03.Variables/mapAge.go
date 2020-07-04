fcee033d161f95b0e510b8173eb3e6ade472b068
jerry <erdia22@gmail.com>
2020-07-04 18:31:53 +0900

    add map variables

---
+++ src/03.Variables/mapAge.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	birthdays := map[string]string{
+		"Keith": "02/06/1990",
+		"Kevin": "01/01/1957",
+		"Kayla": "06/24/1975",
+	}
+
+	fmt.Println(birthdays)
+
+	ages := map[string]int{}
+	ages["Keith"] = 28
+	ages["Kevin"] = 61
+	ages["Kayla"] = 43
+
+	fmt.Println(ages, ages["Kevin"])
+}
--- src/rune.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println('A')
-	fmt.Println("한국어도물론 가능하겠네")
-}
